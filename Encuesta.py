f7d24d61b41c715375bb69d67bf1cdb3cca45d7d
Taína Targelia Sánchez Valencia <168025194+TargeliaSanchez@users.noreply.github.com>
2025-06-26 02:53:26 -0500

    Update Encuesta.py

--- Encuesta.py
+++ Encuesta.py
@@ -1692,17 +1692,17 @@ elif st.session_state.paso == 3:
     if notas_d11[0]:
         with st.expander("Nota"):
             st.markdown(notas_d11[0])
-    
-        
-    for i, texto in enumerate(preguntas_d11):
-        col1, col2= st.columns([4, 1])
-        with col1:
-            st.markdown(texto)
+    #Toby
+    with st.container():    
+        for i, texto in enumerate(preguntas_d11):
+            col1, col2= st.columns([4, 1])
+            with col1:
+                st.markdown(texto)
             #st.markdown("------------------------------")
     
-        with col2:
-            val = st.selectbox("",opciones,format_func=lambda x: x[0],key=f"pD1_1_{i+1}")
-            guardar_respuesta(f"pD1_1_{i+1}", val[1])
+            with col2:
+                val = st.selectbox("",opciones,format_func=lambda x: x[0],key=f"pD1_1_{i+1}")
+                guardar_respuesta(f"pD1_1_{i+1}", val[1])
 
     with st.container():
         col1, col2 = st.columns([1, 4])
