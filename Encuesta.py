6efd6da79aac36cf46fc4b10d29bbe603c2ec83d
Taína Targelia Sánchez Valencia <168025194+TargeliaSanchez@users.noreply.github.com>
2025-07-09 17:55:46 -0500

    Update Encuesta.py

--- Encuesta.py
+++ Encuesta.py
@@ -1,4 +1,5 @@
 ############ Instalación de paquetes
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 from docx import Document
 from docx.shared import Inches
 from docx.oxml import OxmlElement
@@ -17,6 +18,44 @@ from datetime import date
 import json
 import re
 from collections import defaultdict
+#--------------------------------------------------------------------------------------------
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import pickle
+
+def upload_to_drive(local_path, drive_filename, mimetype):
+    SCOPES = ['https://www.googleapis.com/auth/drive.file']
+    creds = None
+    # Usa token.pickle para evitar autenticación cada vez
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('drive', 'v3', credentials=creds)
+    file_metadata = {'name': drive_filename}
+    media = MediaFileUpload(local_path, mimetype=mimetype)
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    return file.get('id')
+    #-------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
 
 ####   Personalización para tabla de exportación
 
