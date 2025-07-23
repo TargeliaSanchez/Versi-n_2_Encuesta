457d618956749fdb88ddc79209a57192aeafd0c1
Taína Targelia Sánchez Valencia <168025194+TargeliaSanchez@users.noreply.github.com>
2025-07-23 09:24:18 -0500

    Update Encuesta.py

--- Encuesta.py
+++ Encuesta.py
@@ -668,7 +668,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.image("Logo_ideal.png",width=250)
+st.image("Logo_ideal.png",width=750)
 
 #st.title("EVALUAR – BPS \n  **EVALUACIÓN DE CONDICIONES ESENCIALES DEL ENFOQUE BIOPSICOSOCIAL EN SERVICIOS DE REHABILITACIÓN**")
 st.markdown("""
