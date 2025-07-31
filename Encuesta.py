77fdac996588eab1c2e45ad94774a37ef471246e
Taína Targelia Sánchez Valencia <168025194+TargeliaSanchez@users.noreply.github.com>
2025-07-31 08:44:26 -0500

    Update Encuesta.py

--- Encuesta.py
+++ Encuesta.py
@@ -706,22 +706,14 @@ if st.session_state.paso == 1:
     
     col1, col2 = st.columns([4,4])
     with col1:
-        departamento = st.text_input(
-            "DEPARTAMENTO",
+        st.markdown("**DEPARTAMENTO**")
+        departamento=st.text_input(
+            "DEPARTAMENTO", 
             value=st.session_state.respuestas.get("departamento", ""),
+            label_visibility="collapsed", 
             key="departamento"
         )
-       guardar_respuesta("departamento", departamento)
-        
-        
-        #st.markdown("**DEPARTAMENTO**")
-        #departamento=st.text_input(
-        #    "DEPARTAMENTO", 
-        #    value=st.session_state.respuestas.get("departamento", ""),
-        #    label_visibility="collapsed", 
-        #    key="departamento"
-        #)
-        #guardar_respuesta("departamento", departamento)
+        guardar_respuesta("departamento", departamento)
     with col2:
         st.markdown("**MUNICIPIO**")
         municipio=st.text_input(
