e5dc4cfb7dde0604b4ab31acca8257aa9541ec05
Taína Targelia Sánchez Valencia <168025194+TargeliaSanchez@users.noreply.github.com>
2025-08-06 11:02:58 -0500

    Update Encuesta.py

--- Encuesta.py
+++ Encuesta.py
@@ -1237,14 +1237,15 @@ if st.session_state.paso == 1:
 
 
     for i in range(1, 3):
+        key=f"prof_verif_{i}"
         prof = st.text_input(
-            label="",
+            "",
             placeholder=f"{i}. Digite nombre completo", 
-            key=f"prof_verif_{i}",
-            value = st.session_state.respuestas.get(f"prof_verif_{i}", ""),
+            key=key,
+            value = st.session_state.respuestas.get(key, ""),
         )
-        st.session_state.respuestas[f"prof_verif_{i}"] = prof
-        guardar_respuesta(f"prof_verif_{i}", prof)
+        st.session_state.respuestas[key] = prof
+        guardar_respuesta(key, prof)
 
     #---------------------------------------------------
     #col1, col2 = st.columns([5, 1])
