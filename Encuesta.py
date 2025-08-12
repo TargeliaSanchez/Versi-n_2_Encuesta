9eb98c575584ebddda28ff0fe5d426e54de7f022
Taína Targelia Sánchez Valencia <168025194+TargeliaSanchez@users.noreply.github.com>
2025-08-12 14:19:03 -0500

    Update Encuesta.py

--- Encuesta.py
+++ Encuesta.py
@@ -5045,10 +5045,10 @@ elif st.session_state.paso == 33:
             label = f"{left+1}-{left+10}" if left != 0 else "1-10"
             ax.text(left + width/2, 0.6, label, ha='center', va='bottom', fontsize=9)
         #ax.plot(valor, 0, 'o', markersize=15, markeredgecolor='black', markerfacecolor='none')
-        ax.text(valor, 0, f'{valor:.1f}', ha='center', va='center', fontsize=8, weight='bold')
+        ax.text(valor, 0, f'{valor:.1f}', ha='center', va='center', fontsize=7, weight='bold')
 
-        #ax.set_xlim(0, 100)
-        ax.set_xlim(0, 105)
+        ax.set_xlim(0, 100)
+        #ax.set_xlim(0, 105)
         ax.set_ylim(-1.2, 1.2)  # 👈 Ajusta aquí para recortar el espacio arriba
         ax.axis('off')
 
